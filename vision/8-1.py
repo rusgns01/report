2f4c3d6903c6c3629196b682400ea8f4ee25ae17
rusgns01 <rusgns01@naver.com>
2025-06-20 23:15:34 +0900

    report
    report

---
+++ vision/8-1.py
@@ -0,0 +1,32 @@
+import numpy as np
+import tensorflow as tf
+import tensorflow.keras.datasets as ds
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dropout, Dense
+from tensorflow.keras.optimizers import Adam, SGD
+
+(x_train, y_train), (x_test, y_test) = ds.mnist.load_data()
+x_train = x_train.reshape(60000, 28, 28, 1)
+x_test = x_test.reshape(10000, 28, 28, 1)
+x_train = x_train.astype(np.float32) / 255.0
+x_test = x_test.astype(np.float32) / 255.0
+y_train = tf.keras.utils.to_categorical(y_train, 10)
+y_test = tf.keras.utils.to_categorical(y_test, 10)
+
+cnn = Sequential()
+cnn.add(Conv2D(6, (5, 5), padding='same', activation='relu', input_shape=(28, 28, 1)))
+cnn.add(MaxPooling2D(pool_size=(2, 2), strides=2))
+cnn.add(Conv2D(16, (5, 5), padding='valid', activation='relu'))
+cnn.add(MaxPooling2D(pool_size=(2, 2), strides=2))
+cnn.add(Conv2D(120, (5, 5), padding='valid', activation='relu'))
+cnn.add(Flatten())
+cnn.add(Dropout(0.5))  # Flatten 후 또는 Dense 사이에 삽입
+cnn.add(Dense(units=84, activation='relu'))
+cnn.add(Dense(units=10, activation='softmax'))
+
+cnn.compile(loss='categorical_crossentropy', optimizer=SGD(learning_rate=0.01, momentum=0.9), metrics=['accuracy'])
+cnn.fit(x_train, y_train, batch_size=128, epochs=30, validation_data=(x_test, y_test), verbose=2)
+
+res = cnn.evaluate(x_test, y_test, verbose=0)
+print('정확률=', res[1] * 100)
