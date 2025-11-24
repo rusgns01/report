b346faf41dbabaf8fd7b7f55e686628ad400e627
rusgns01 <rusgns01@naver.com>
2025-11-24 23:47:34 +0900

    test

---
+++ CodeTest/코드14번/코드14번/14번.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+struct dat {
+    int x;
+    int y;
+};
+
+int main() {
+    struct dat a[] = { {1, 2}, {3, 4}, {5, 6} };
+    struct dat* ptr = a;
+    struct dat** pptr = &ptr;
+
+    (*pptr)[1] = (*pptr)[2];
+    printf("%d ±×¸®°í %d", a[1].x, a[1].y);
+
+    return 0;
+}
